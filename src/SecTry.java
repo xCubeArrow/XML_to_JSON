cd9bbca5c7dbb777b2030a8d31ba42b0be2e27b2
xCubeArrow <martinharald.goetze@gmail.com>
2018-04-04 15:34:52 +0200

    Rearranged some code

--- src/SecTry.java
+++ src/SecTry.java
@@ -82,6 +82,41 @@ public class SecTry {
         return returnString[0].substring(0, returnString.length - 1);
     }
 
+    //WORKS
+    static String displayNormal(Element element) {
+        ArrayList<Element> leftOver = new ArrayList<>();
+        final String[] returnString = {""};
+        String name = element.getName();
+        returnString[0] = returnString[0] + "\"" + name + "\":{";
+        returnString[0] += getAttributes(element);
+
+        HashMap<String, String> stuff = new HashMap<>();
+
+        List<Element> children = element.getChildren();
+
+        children.forEach(e -> {
+            if (e.getChildren().size() > 0) {
+                leftOver.add(e);
+
+            } else if (!e.hasAttributes())
+                stuff.put(e.getName(), e.getText());
+            else
+                returnString[0] += "\"" + e.getName() + "\":" + attributesAndContent(e) + ",";
+        });
+
+        stuff.forEach((string3, string2) -> {
+            //TODO:Make different datatypes usable
+
+            returnString[0] = returnString[0] + "\"" + string3 + "\":" + string2 + ",";
+        });
+        if (leftOver.size() > 0)
+            returnString[0] += main(leftOver.toArray(new Element[leftOver.size()]));
+        returnString[0] = returnString[0].substring(0, returnString[0].length() - 1);
+        returnString[0] += "},";
+        return returnString[0].substring(0, returnString[0].length() - 1);
+    }
+
+
     private static String mainWithoutName(Element element) {
 
         String returnString = "{";
@@ -105,6 +140,7 @@ public class SecTry {
         return returnString[0];
     }
 
+
     public static void main(String[] args) {
         File inputFile = new File("C:\\Users\\Martin Goetze\\workspace\\XML to JSON\\res\\xml.xml");
         SAXBuilder saxBuilder = new SAXBuilder();
@@ -124,37 +160,4 @@ public class SecTry {
         }
     }
 
-    //WORKS
-    static String displayNormal(Element element) {
-        ArrayList<Element> leftOver = new ArrayList<>();
-        final String[] returnString = {""};
-        String name = element.getName();
-        returnString[0] = returnString[0] + "\"" + name + "\":{";
-        returnString[0] += getAttributes(element);
-
-        HashMap<String, String> stuff = new HashMap<>();
-
-        List<Element> children = element.getChildren();
-
-        children.forEach(e -> {
-            if (e.getChildren().size() > 0) {
-                leftOver.add(e);
-
-            } else if (!e.hasAttributes())
-                stuff.put(e.getName(), e.getText());
-            else
-                returnString[0] += "\"" + e.getName() + "\":" + attributesAndContent(e);
-        });
-
-        stuff.forEach((string3, string2) -> {
-            //TODO:Make different datatypes usable
-
-            returnString[0] = returnString[0] + "\"" + string3 + "\":" + string2 + ",";
-        });
-        if (leftOver.size() > 0)
-            returnString[0] += main(leftOver.toArray(new Element[leftOver.size()]));
-        returnString[0] = returnString[0].substring(0, returnString[0].length() - 1);
-        returnString[0] += "},";
-        return returnString[0].substring(0, returnString[0].length() - 1);
-    }
 }
